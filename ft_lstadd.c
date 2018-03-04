2bebb1a1e5893f6f153f2ebfa0d7c9167859f86e
Wendy Huang <huanghuang5087@hotmail.com>
2018-03-05 07:22:40 +0800

    Update ft_lstadd.c

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -15,8 +15,7 @@
 void	ft_lstadd(t_list **alst, t_list *new)
 {
 	if (!alst || !new)
-		return ;
-	new->next = *alst;
-	*alst = new;
-	return ;
+		return ;   // 判断无效输入
+	new->next = *alst; // 将 next->next 的值变成 alst所指向的那个东西(指向t_list的指针)一样
+	*alst = new;       // alst所指向的那个东西(指向t_list的指针)变成 和 new一样
 }
